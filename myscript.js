60d2d6f2e6f5f9bcd34addbf06a6f9a6a08031e7
Noah Msekni <msekninoah@gmail.com>
2022-09-09 15:37:59 +0200

    Add in game rules, display winner or tie
    As the users place their marks check if anyone won or if its a tie.
    if so display it in the statusmessage.

--- myscript.js
+++ myscript.js
@@ -5,6 +5,42 @@ const gameboard = {
   board: ['square1', 'square2', 'square3', 'square4', 'square5', 'square6', 'square7', 'square8', 'square9']
 }
 
+const gamePlay = () => {
+  let horiCombo1 = gameboard.board[0] + gameboard.board[1] +gameboard.board[2]
+  let horiCombo2 = gameboard.board[3] + gameboard.board[4] +gameboard.board[5]
+  let horiCombo3 = gameboard.board[6] + gameboard.board[7] +gameboard.board[8]
+  
+  let verCombo1 = gameboard.board[0] + gameboard.board[3] +gameboard.board[6]
+  let verCombo2 = gameboard.board[1] + gameboard.board[4] +gameboard.board[7]
+  let verCombo3 = gameboard.board[2] + gameboard.board[5] +gameboard.board[8]
+
+
+  let diaCombo1 = gameboard.board[0] + gameboard.board[4] +gameboard.board[8]
+  let diaCombo2 = gameboard.board[2] + gameboard.board[4] +gameboard.board[6]
+  
+  if(diaCombo1 == 'xxx' || diaCombo2 == 'xxx' || verCombo1 == 'xxx' || verCombo2 == 'xxx' || verCombo3 == 'xxx' || horiCombo1 == 'xxx' || horiCombo2 == 'xxx' || horiCombo3 == 'xxx'){
+    statusMessage.textContent = players.player1.username + " won!"
+
+  }
+  else if(diaCombo1 == 'ooo' || diaCombo2 == 'ooo' || verCombo1 == 'ooo' || verCombo2 == 'ooo' || verCombo3 == 'ooo' || horiCombo1 == 'ooo' || horiCombo2 == 'ooo' || horiCombo3 == 'ooo'){
+    console.log('noob won!')
+    statusMessage.textContent = players.player2.username + " won!"
+  } else {
+    
+
+    function ifTie(square){
+      return square == 'x' || square == 'o'
+    }
+    if(gameboard.board.every(ifTie) === true){
+      console.log('its a tie!')
+      statusMessage.textContent = 'its a tie!'
+    }
+
+      
+      }
+    }
+
+
 
 
 
@@ -69,7 +105,7 @@ const displayController = () => {
 
 
   window.onclick = e => {
-    console.log(e.target.className)
+
     const clickedElement = document.querySelector(`.${e.target.className}`)
     console.log(clickedElement)
 
@@ -86,7 +122,7 @@ const displayController = () => {
         gameboard.board[index] = playerPicker
         console.log(gameboard.board)
         clickedElement.textContent = playerPicker
-
+        gamePlay()
 
     }
   }
